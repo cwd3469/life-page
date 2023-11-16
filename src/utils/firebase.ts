4b9ef12ce6763b857ded57044ae60516c3838973
choi joo young <68840733+cwd3469@users.noreply.github.com>
2023-11-16 20:20:02 +0900

    main feat : env 설정 적용

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,21 +1,21 @@
 import { getApp, getApps, initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getDatabase } from "firebase/database";
-
+import { getFirestore } from "firebase/firestore";
+//TODO:추후 env로 이동할 예정
 const FirebaseConfig = () => {
   const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_apiKey,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_authDomain,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_projectId,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_storageBucket,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_messagingSenderId,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_appId,
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_measurementId,
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGINGSENDERID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENTID,
   };
 
   const app = initializeApp(firebaseConfig);
-  const db = getDatabase(app);
-  return db;
+  const db = getFirestore(app);
+  return { db };
 };
 
 export default FirebaseConfig;
