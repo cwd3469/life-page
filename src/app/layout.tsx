77940e06598a226b091f44d04d056ff65f94e8bb
choi joo young <68840733+cwd3469@users.noreply.github.com>
2023-11-28 23:18:28 +0900

    main feat : typography 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <div className="absolute top-1/2 left-1/2 -translate-x-2/4	-translate-y-2/4 ">
+        <div className="absolute top-1/2 left-1/2 -translate-x-2/4	-translate-y-2/4 bg-slate-600 lg:w-full">
           {children}
         </div>
       </body>
