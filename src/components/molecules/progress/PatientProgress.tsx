b2edc222e8c53aa38b635bdd4738a3992842f609
choi joo young <68840733+cwd3469@users.noreply.github.com>
2023-12-06 13:55:11 +0900

    PatientProgress head 프로그레스 바 개발

---
+++ src/components/molecules/progress/PatientProgress.tsx
@@ -0,0 +1,27 @@
+'use client';
+import { useModelingQuestion } from '../../../controller/QuestionnaireDataContext';
+
+const PatientProgress = () => {
+  const { progress } = useModelingQuestion();
+  const label = progress.label.split('/');
+  const current = label.length ? label[0] : '';
+  const total = label.length ? label[1] : '';
+  return (
+    <div className="py-[18px] px-4 flex flex-col gap-2 ">
+      <div className="flex justify-between ">
+        <p className="font-semibold text-sm text-[#484848] tracking-tight">
+          <span>문진 작성 진행율</span>
+          <span className="text-[#1ABCB7]  ml-2">{progress.percent}%</span>
+        </p>
+        <div className="leading-5	align-middle text-xs text-[#8F8F8F] px-2">
+          <span className="text-[#1ABCB7]">{current}</span>/<span>{total}</span>
+        </div>
+      </div>
+      <div className="w-full bg-[#F7F7F7] rounded-full h-2">
+        <div className="bg-[#1ABCB7] h-2 rounded-full" style={{ width: `${progress.percent}%` }}></div>
+      </div>
+    </div>
+  );
+};
+
+export default PatientProgress;
